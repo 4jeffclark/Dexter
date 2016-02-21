40f075d2dd8077c22b9099eae34a872796d32dda
Jeff <4jeffclark@gmail.com>
2016-02-21 14:38:41 -0700

    took out io stuff

--- totalBeginner/src/org/totalbeginner/tutorial/LibraryApp.java
+++ totalBeginner/src/org/totalbeginner/tutorial/LibraryApp.java
@@ -10,36 +10,11 @@ import java.util.Scanner;
 public class LibraryApp {
 
 	public static void main(String[] args) {
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		Console console=System.console();
-	    String username = null;
 	    
 	    
 		Person p = new Person();
 		System.out.println(p.getName());
 		
-		
-	    System.out.print("Please enter user name : ");
-	    try {
-	        username = reader.readLine();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	    System.out.println("You entered : " + username);
-	    
-        System.out.println(System.getProperty("user.dir"));
-        File file = new File("file.txt");
-        try {
-            Scanner sc = new Scanner(file);
-
-            while (sc.hasNextLine()) {
-                int i = sc.nextInt();
-                System.out.println(i);
-            }
-            sc.close();
-        } catch (Exception e) {
-        System.out.println(e);
-        }
 
 	}
 	
